3d03d4043702879fc39af733428a80f2866a8862
pankajsharmahome <herespankaj@gmail.com>
2018-07-20 21:10:46 +0100

    setup of the Express js server

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,23 @@
 
-console.log('Hello !!!');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const cors          = require('cors');
+const morgan        = require('morgan');
+
+const app = express(); // builds an express server 
+
+app.use(morgan('combine'));
+app.use(bodyParser.json());
+app.use(cors()); 
+
+app.get('/status', (req, res) => { 
+
+    res.send({
+        message: 'Hello World !!!'
+    })
+})
+
+app.listen(process.env.PORT || 8081);
+
+
+
