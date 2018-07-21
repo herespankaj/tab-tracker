ae9057446f1051ec0dbc0f71ccb921d47d5c7dae
pankajsharmahome <herespankaj@gmail.com>
2018-07-21 19:03:07 +0100

    Created Login/ Registration page and hit the register end point

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,3 @@
-
 const express       = require('express');
 const bodyParser    = require('body-parser');
 const cors          = require('cors');
@@ -6,18 +5,24 @@ const morgan        = require('morgan');
 
 const app = express(); // builds an express server 
 
-app.use(morgan('combine'));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.use(cors()); 
-
-app.get('/status', (req, res) => { 
-
-    res.send({
-        message: 'Hello World !!!'
-    })
-})
 
 app.listen(process.env.PORT || 8081);
 
+app.post('/register', (req, res) => { 
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    res.send({
+
+        message: 'Hello ' + email + ' Your user was registered. Have fun !!! \n Your Password is ==>' + password
+
+    });
+})
 
 
